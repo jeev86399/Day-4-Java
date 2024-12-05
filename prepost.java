94fa5c3c484dea17f46ef2b2322db04a1019c99e
jeev86399 <nnani86399@gmail.com>
2024-12-05 16:04:37 +0530

    Add files via upload

---
+++ prepost.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class prepost{
+    public static void main(String args[]){
+    int a=5;
+    System.out.println(a++);//post increament
+    int b=4;
+    System.out.println(++b);//pre increment
+    int n=12;
+    System.out.println(n--);//post decrement
+    int a1=56;
+    System.out.println(--a1);//pre decrement
+
+    }
+}
