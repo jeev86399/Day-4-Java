44773c5b1281bd388095a059011959768dec706f
jeev86399 <nnani86399@gmail.com>
2024-12-05 16:24:17 +0530

    Add files via upload

---
+++ revn.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class revn{
+    public static void main(String args[])
+    {
+        int n=10;
+        int i=n-1;
+        while(i>=0)
+        {
+            System.out.println(i);
+            i-=1;
+        }
+    }
+}
